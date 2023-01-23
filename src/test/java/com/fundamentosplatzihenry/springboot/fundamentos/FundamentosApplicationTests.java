e8ccec1a50ac53342ee776bfc99664a25b4cbff1
Henry Infante G <henryinfanteg@gmail.com>
2023-01-22 23:29:20 -0500

    my first commit

---
+++ src/test/java/com/fundamentosplatzihenry/springboot/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzihenry.springboot.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
